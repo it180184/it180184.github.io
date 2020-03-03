a2b8fa88769d55e0d40a8ea678eb9b59c03916ca
it180184 <f.ettinger@students.htl-leonding.ac.at>
2020-03-03 14:40:10 +0100

    added image slideshow

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-let images = [
+const images = [
     'java.png',
     'html_css_javascript.png',
     'cc.png',
     'msoffice.jpg'
 ];
 
+const image = document.getElementById('logos');
+
+let current = 3;
+
+function changeImage() {
+
+    current++;
+
+    if (current == images.length) {
+        current = 0;
+    }
+
+    image.src = 'img/' + images[current];
+};
+
+setInterval(changeImage, 4000);
+
+changeImage();
